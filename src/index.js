6f1b7730be6103864204e8681cf1dcd60bf1ef9c
Melissa Guachun <1212malka@gmail.com>
2021-08-10 13:50:18 -0400

    debug

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// const baseUrl = "http://localhost:3000/genres/";
-// const resultsUl = document.getElementById('results');
-// // const button = document.getElementById('search-btn');
-// const albumUl = document.getElementById('album-list');
-// const genreUl = document.getElementById('genre-list');
-
-
  document.addEventListener('DOMContentLoaded', () => {
     fetchAlbums();
     albumForm.addEventListener('submit', postAlbum);
@@ -17,7 +10,7 @@
   
     switch(eventButton.innerText) {
       case 'Delete': 
-        deleteAlbum(albumId, bookContainer);
+        deleteAlbum(albumId, albumContainer);
         break;
       case 'Save':
         eventButton.innerText = 'Edit';
@@ -59,40 +52,5 @@ function fetchAlbums(){
     })
 }
 
-// //create a new album and append it to the DOM
-// const postAlbum = () => {
-//     event.preventDefault();
-//     const formData = {
-//         title: titleInput.value,
-//         artist: artistInput.value,
-//         year: yearInput.value,
-//         genre: genreInput.value
-//     };
 
-//     const configObj = {
-//         method: 'POST',
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json"
-//         },
-//         body: JSON.stringify(formData)
-//     };
 
-//     fetch(baseUrl, configObj)
-//     .then(resp => resp.json())
-//     .then(book => {
-//         renderAlbum(album);
-//         albumForm.reset();
-//     });
-// };
-
-// function splitQuery(query){
-//     return query.replace(' ', '+');
-// };
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     // button.addEventListener('click', searchAlbums);
-//     fetchGenres()
-//     fetchAlbums()
-
-//  });
