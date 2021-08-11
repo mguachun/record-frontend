3ad603fb6d32e3400531aefe66562bbdcb5bb72d
Melissa Guachun <1212malka@gmail.com>
2021-08-10 20:48:21 -0400

    recereated album class

---
+++ src/album.js
@@ -0,0 +1,11 @@
+class Album {
+    constructor({id, title, artist, year, genre}){
+    this.id = id;
+    this.title = title;
+    this.artist = artist;
+    this.year = year;
+    this.genre = genre;
+    }
+
+
+};
