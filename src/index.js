cb55db2165026ee1421c8ed3f252fa6b50ca49be
Melissa Guachun <1212malka@gmail.com>
2021-08-10 20:49:47 -0400

    cleared index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
- document.addEventListener('DOMContentLoaded', () => {
-    fetchAlbums();
-    albumForm.addEventListener('submit', postAlbum);
-  });
- 
-  const handleAlbumClick = () => {
-    const albumContainer = event.target.parentElement.parentElement;
-    const albumId = albumContainer.dataset.id;
-    const eventButton = event.target;
-  
-    switch(eventButton.innerText) {
-      case 'Delete': 
-        deleteAlbum(albumId, albumContainer);
-        break;
-      case 'Save':
-        eventButton.innerText = 'Edit';
-        editAlbum(albumId, albumContainer);
-        break;
-      default: 
-        console.log('No changes were made.');
-        break;
-    };
-  
-  };
 
-    function splitQuery(query){
-      return query.replace(' ', '+');
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  document.addEventListener('DOMContentLoaded', () => {
+//     fetchAlbums();
+//     albumForm.addEventListener('submit', postAlbum);
+//   });
+ 
+//   const handleAlbumClick = () => {
+//     const albumContainer = event.target.parentElement.parentElement;
+//     const albumId = albumContainer.dataset.id;
+//     const eventButton = event.target;
+  
+//     switch(eventButton.innerText) {
+//       case 'Delete': 
+//         deleteAlbum(albumId, albumContainer);
+//         break;
+//       case 'Save':
+//         eventButton.innerText = 'Edit';
+//         editAlbum(albumId, albumContainer);
+//         break;
+//       default: 
+//         console.log('No changes were made.');
+//         break;
+//     };
+  
+//   };
+
+//     function splitQuery(query){
+//       return query.replace(' ', '+');
+//     };
 
 
 
