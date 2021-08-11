48083642a6900a7ba239920295152c494f9a0321
Melissa Guachun <1212malka@gmail.com>
2021-08-10 20:48:33 -0400

    recreared album services

---
+++ src/albumservices.js
@@ -0,0 +1,17 @@
+class AlbumServices {
+    constructor(baseUrl){
+        this.baseUrl = baseUrl;
+    };
+    // the fetch doesn't work in here 
+    // function fetchAlbums(){
+    // fetch('http://localhost:3000/albums')
+    // .then(resp => resp.json())
+    // .then(data => {
+    //     data.forEach(album => {
+    //       const li = document.createElement('li')
+    //       li.innerText = album.title;
+    //       albumUl.appendChild(li);
+    //     })
+    // })
+    // }
+}
