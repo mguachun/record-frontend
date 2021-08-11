31784038cdf89f8406b9b1d0e4b873078f2b3e26
Melissa Guachun <1212malka@gmail.com>
2021-08-10 20:48:47 -0400

    commented out code

--- src/api.js
+++ src/api.js
@@ -25,49 +25,49 @@ function fetchAlbums(){
     })
 }
 
-// Create a new album and append it to the DOM
-const postAlbum = () => {
-    event.preventDefault();
+// // Create a new album and append it to the DOM
+// const postAlbum = () => {
+//     event.preventDefault();
 
-    const formData = {
-        title: titleInput.value,
-        artist: artistInput.value,
-        year: yearInput.value,
-        genre: genreInput.value
-    };
+//     const formData = {
+//         title: titleInput.value,
+//         artist: artistInput.value,
+//         year: yearInput.value,
+//         genre: genreInput.value
+//     };
 
-    const configObj = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(formData)
-    };
+//     const configObj = {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(formData)
+//     };
 
-    //method="POST" action="/albums"
-    fetch(baseUrl, configObj)
-    .then(resp => resp.json())
-    .then(book => {
-        renderAlbum(album);
-        albumForm.reset();
-    });
-};
+//     //method="POST" action="/albums"
+//     fetch(baseUrl, configObj)
+//     .then(resp => resp.json())
+//     .then(book => {
+//         renderAlbum(album);
+//         albumForm.reset();
+//     });
+// };
 
-// Delete an album and remove it from the DOM.
-const deleteAlbum = (id, albumEl) => {
-    const configObj = {
-      method: 'DELETE',
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      }
-    };
+// // Delete an album and remove it from the DOM.
+// const deleteAlbum = (id, albumEl) => {
+//     const configObj = {
+//       method: 'DELETE',
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       }
+//     };
   
-    fetch(`${baseUrl}/${id}`, configObj)
-    albumEl.remove();
-  };
+//     fetch(`${baseUrl}/${id}`, configObj)
+//     albumEl.remove();
+//   };
 
-function splitQuery(query){
-    return query.replace(' ', '+');
-};
+// function splitQuery(query){
+//     return query.replace(' ', '+');
+// };
