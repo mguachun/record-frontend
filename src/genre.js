3fac7d7e7522f4c1cb9800d95e23b3eb8799ebf7
Melissa Guachun <1212malka@gmail.com>
2021-08-12 21:07:20 -0400

    render genre, genre info, and album genres

--- src/genre.js
+++ src/genre.js
@@ -3,4 +3,36 @@ class Genre {
         this.id = id;
         this.name = name;
     }
-}
+
+    renderGenre() {
+        const genreCard = document.createElement('div');
+        genreCard.classList.add('album-card');
+
+        const genreInfo = this.renderGenreInfo();
+        const albums = this.renderAlbumGenres();
+
+        genreCard.appendChild(genreInfo);
+        genreCard.appendChild(genres);
+        Genre.container.appendChild(genreCard);
+    };
+
+    renderGenreInfo() {
+        const genreData = document.createElement('div');
+
+        genreData.innerHTML = `
+        <h1>${this.name}</h1>
+        `;
+
+        return genreData;
+    };
+
+    renderAlbumGenres() {
+        const albumUl = document.createElement('ul');
+        albumUl.classList.add('albums');
+
+        this.albums.forEach(album => {
+            const newAlbum = new Album(album);
+            const li = newAlbum.renderAlbum();
+        });
+    };
+};
