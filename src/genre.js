9e1bcb3088be03f3feb4258ec651dc4154636f01
Melissa Guachun <1212malka@gmail.com>
2021-08-13 15:31:03 -0400

    reconfig fetch

--- src/genre.js
+++ src/genre.js
@@ -4,49 +4,22 @@ class Genre {
         this.name = name;
     }
 
-    // Retrieve all genres from backend and append to the DOM
-     fetchGenres(){
-    fetch('http://localhost:3000/genres')
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(genre => {
-        //   console.log(genre.name)
-        const li = document.createElement('li')
-        li.innerText = genre.name;
-        genreUl.appendChild(li);
-        })
-    })
-}
-
     renderGenre() {
-        const genreCard = document.createElement('div');
-        genreCard.classList.add('album-card');
+        const li = document.createElement('li');
+        const deleteBtn = document.createElement('button');
 
-        const genreInfo = this.renderGenreInfo();
-        const albums = this.renderAlbumGenres();
+        li.innerText = this.name;
 
-        genreCard.appendChild(genreInfo);
-        genreCard.appendChild(genres);
-        Genre.container.appendChild(genreCard);
-    };
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.innerText = 'x'
 
-    renderGenreInfo() {
-        const genreData = document.createElement('div');
+        deleteBtn.addEventListener('click', () => {
+            console.log(this, "I've been clicked!");
 
-        genreData.innerHTML = `
-        <h1>${this.name}</h1>
-        `;
-
-        return genreData;
-    };
-
-    renderAlbumGenres() {
-        const albumUl = document.createElement('ul');
-        albumUl.classList.add('albums');
-
-        this.albums.forEach(album => {
-            const newAlbum = new Album(album);
-            const li = newAlbum.renderAlbum();
         });
+        li.appendChild(deleteBtn);
+
+        return li;
     };
+
 };
