b88549448232eb3dbbed7b159e9b49ca96399803
Melissa Guachun <1212malka@gmail.com>
2021-08-13 14:18:48 -0400

    coded out api.js

--- src/api.js
+++ src/api.js
@@ -1,29 +1,29 @@
 // Retrieve all genres from backend and append to the DOM
-function fetchGenres(){
-    fetch('http://localhost:3000/genres')
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(genre => {
-        //   console.log(genre.name)
-        const li = document.createElement('li')
-        li.innerText = genre.name;
-        genreUl.appendChild(li);
-        })
-    })
-}
+// function fetchGenres(){
+//     fetch('http://localhost:3000/genres')
+//     .then(resp => resp.json())
+//     .then(data => {
+//         data.forEach(genre => {
+//         //   console.log(genre.name)
+//         const li = document.createElement('li')
+//         li.innerText = genre.name;
+//         genreUl.appendChild(li);
+//         })
+//     })
+// }
 
-// Retrieve all albums from backend and append to the DOM
-function fetchAlbums(){
-    fetch('http://localhost:3000/albums')
-    .then(resp => resp.json())
-    .then(data => {
-        data.forEach(album => {
-          const li = document.createElement('li')
-          li.innerText = album.title;
-          albumUl.appendChild(li);
-        })
-    })
-}
+// // Retrieve all albums from backend and append to the DOM
+// function fetchAlbums(){
+//     fetch('http://localhost:3000/albums')
+//     .then(resp => resp.json())
+//     .then(data => {
+//         data.forEach(album => {
+//           const li = document.createElement('li')
+//           li.innerText = album.title;
+//           albumUl.appendChild(li);
+//         })
+//     })
+// }
 
 // // Create a new album and append it to the DOM
 // const postAlbum = () => {
