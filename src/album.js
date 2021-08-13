164aa458170b2e49e070aeb9ff6710abdd31488a
Melissa Guachun <1212malka@gmail.com>
2021-08-12 21:06:38 -0400

    commented out genre in album attrs

--- src/album.js
+++ src/album.js
@@ -1,11 +1,45 @@
+const albumApi = new AlbumServices('http://localhost:3000/albums');
+
 class Album {
-    constructor({id, title, artist, year, genre}){
-    this.id = id;
-    this.title = title;
-    this.artist = artist;
-    this.year = year;
-    this.genre = genre;
-    }
+    static container = document.getElementById('album-attr-list');
+    constructor({ id, title, artist, year, genre_id }) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+        // this.genre = genre;
+        this.genre_id = genre_id
+    };
 
+    renderAlbum() {
+        const li = document.createElement('li');
+        const deleteBtn = document.createElement('button');
 
+        li.innerText = this.name;
+
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.innerText = 'x';
+
+        deleteBtn.addEventListener('click', () => {
+            console.log(this, "I've been clicked!");
+        });
+
+        li.appendChild(deleteBtn);
+
+        return li;
+
+    };
+
+    renderAlbumInfo(){
+        const albumData = document.createElement('div');
+
+        albumData.innerHTML = `
+        <h1>${this.title}</h1>
+        <p>${this.artist}</p>
+        <p>${this.year}</p>
+        `;
+
+        return albumData;
+
+    };
 };
