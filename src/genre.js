3c8bcd0f1684ee72d09d51fb28565e96a81a0d16
Melissa Guachun <1212malka@gmail.com>
2021-08-16 23:34:35 -0400

    commented out render genre

--- src/genre.js
+++ src/genre.js
@@ -4,22 +4,23 @@ class Genre {
         this.name = name;
     }
 
-    renderGenre() {
-        const li = document.createElement('li');
-        const deleteBtn = document.createElement('button');
+    
+    // renderGenre() {
+    //     const li = document.createElement('li');
+    //     const deleteBtn = document.createElement('button');
 
-        li.innerText = this.name;
+    //     li.innerText = this.name;
 
-        deleteBtn.classList.add('delete-btn');
-        deleteBtn.innerText = 'x'
+    //     deleteBtn.classList.add('delete-btn');
+    //     deleteBtn.innerText = 'x'
 
-        deleteBtn.addEventListener('click', () => {
-            console.log(this, "I've been clicked!");
+    //     deleteBtn.addEventListener('click', () => {
+    //         console.log(this, "I've been clicked!");
 
-        });
-        li.appendChild(deleteBtn);
+    //     });
+    //     li.appendChild(deleteBtn);
 
-        return li;
-    };
+    //     return li;
+    // };
 
 };
