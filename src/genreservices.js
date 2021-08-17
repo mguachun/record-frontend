65eb077bb63b8100a5cc1cbdc7ba8716fb056912
Melissa Guachun <1212malka@gmail.com>
2021-08-16 23:11:11 -0400

    commented out genremenu

--- src/genreservices.js
+++ src/genreservices.js
@@ -3,6 +3,21 @@ class GenreServices {
         this.baseUrl = baseUrl;
     };
 
+    // renderGenreMenu(){
+    //     const genreMenu = document.createElement('div');
+
+    //     genreMenu.classList.add('genre-menu');
+    //     genreMenu.innerText = 'Select Genre'
+    
+    //     genreMenu.addEventListener('click', () => {
+    //         console.log("click");
+
+    //     });
+    //     // li.appendChild(deleteBtn);
+
+    //     // return li;
+    // }
+
   
 };
 
