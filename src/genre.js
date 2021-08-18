81cd4fadb64424f329fc81545cb87e6db2e039d4
Melissa Guachun <1212malka@gmail.com>
2021-08-18 12:47:41 -0400

    destructuring

--- src/genre.js
+++ src/genre.js
@@ -1,31 +1,17 @@
 class Genre {
-    constructor(id, name) {
+    constructor({id, name}) {
         this.id = id;
         this.name = name;
     }
 
     
     renderGenre() {
-        // debugger
         const genreSelect = document.getElementById('genre-menu');
         const genreCard = document.createElement('option');
 
         genreCard.text = this.name 
-        genreCard.innerHTML= 
-        // this.name = $(input[this.name]);
-        // genreCard.value = this.name
 
-        // genreCard.options.add(option)
-
-        // genreCard.classList.add('genre-card');
-
-        // const genreName = document.createElement('h1');
-        // genreName.innertext = this.name;
-
-        // genreCard.appendChild(genreName);
-        // genreName.addEventListener('click', ()=>{
-          
-        // })
+    
         genreSelect.appendChild(genreCard);
     };
 
