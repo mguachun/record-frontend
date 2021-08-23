9f3a10bb6691ff5995b69d15837a7b1348e91e92
Melissa Guachun <1212malka@gmail.com>
2021-08-23 18:54:02 -0400

    consolidated instances and event listeners

--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 const albumApi = new AlbumServices('http://localhost:3000/albums')
-
+const genreApi = new GenreServices('http://localhost:3000/genres')
+const albumForm = document.getElementById('album-form');
 document.addEventListener('DOMContentLoaded', ()  => {
     albumApi.getAlbums();
-});
-
-
-const genreApi = new GenreServices('http://localhost:3000/genres')
-
-document.addEventListener('DOMContentLoaded', ()  => {
     genreApi.getGenres();
+    albumForm.addEventListener('submit', albumApi.postAlbum);
+
 });
 
-// add event listener for adding album
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     albumApi.createAlbum();
-// });
 
 
 
