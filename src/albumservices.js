5186ade747e806fc9556de9a3a13a1df87453506
Melissa Guachun <1212malka@gmail.com>
2021-09-02 12:01:32 -0400

    live coding

--- src/albumservices.js
+++ src/albumservices.js
@@ -71,10 +71,14 @@ class AlbumServices {
     renderWordSearch() {
         const albumApi = new AlbumServices('http://localhost:3000/albums')
     //    const result = albumApi.getAlbums()
-        const result = getAlbum.renderAlbum();
+        // const result = albumApi.getAlbums(album=> albumName.length > 3);
     //    result.filter(album => albumName.length > 3);
+        // let albums = []
+        const listAlbums = ["The Dark Side of the Moon", "Born in the U.S.A", "Nevermind", "Purple Rain", 
+        "The Miseducation of Lauryn Hill"]
+        listAlbums.filter( album => albumName.length > 3);
 
-        console.log(result);
+        console.log(listAlbums);
     
     }
     }
