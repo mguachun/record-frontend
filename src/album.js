39f21f8e5bf2aad869281785baa0b0e8acc42d49
Melissa Guachun <1212malka@gmail.com>
2022-04-03 00:13:17 -0400

    static and push

--- src/album.js
+++ src/album.js
@@ -1,5 +1,7 @@
 
 class Album {
+
+    static all = [];
     constructor({ artist, genre_id, year, genre, title, id}){
         this.artist = artist;
         this.genre_id = genre_id;
@@ -7,6 +9,7 @@ class Album {
         this.genre = genre;
         this.title = title;
         this.id = id;
+        Album.all.push(this);
     };
 
     renderAlbum() {
@@ -53,6 +56,8 @@ class Album {
         const deleteBtn = document.createElement('button');
         const albumData = document.createElement('div');
      
+        albumData.classList.add('albumData');
+
     
         deleteBtn.classList.add('delete-btn');
         deleteBtn.innerText = 'Delete Album';
@@ -60,13 +65,52 @@ class Album {
      
 
         albumData.innerHTML = `
+        <br>
         <p>Artist: ${this.artist}</p>
         <p>Year Released:${this.year}</p>
         <p>Genre: ${this.genre.name}</p>
+        <br>
         `;
         albumData.appendChild(deleteBtn);
         return albumData;
     
     };
 
+    static search(event) {
+        event.preventDefault();
+        // const query = Album.all.filter(album => album.id > 20);
+
+        const searchForm = document.getElementById('query');
+        console.log(searchForm.value);
+
+
+        const filterAlbums = Album.all.filter(album => album.title.includes(searchForm.value));
+        console.log(filterAlbums);
+
+        const deleteList = document.getElementById('album-list');
+        deleteList.innerHTML = ' ';
+
+        filterAlbums.forEach(album => {
+        
+            album.renderAlbum();
+        })
+
+    
+
+    }
+
+    
+    
+
 };
+
+
+
+// add a search bar that allows albums to be searched by title
+
+// create an input for search
+// add an event listener to input 
+// define callback function 
+    // filter Album.all with search term
+    // remove all albums from the dom
+    // update with new list of albums
