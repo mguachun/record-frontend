2a63542eaa988addd077853666da5ace98a3c324
Melissa Guachun <1212malka@gmail.com>
2022-04-03 00:13:33 -0400

    search form

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const albumApi = new AlbumServices('http://localhost:3000/albums')
 const genreApi = new GenreServices('http://localhost:3000/genres')
 const albumForm = document.getElementById('album-form');
+const searchForm = document.getElementById('search');
 
 document.addEventListener('DOMContentLoaded', ()  => {
     albumApi.getAlbums();
     genreApi.getGenres();
     albumForm.addEventListener('submit', albumApi.postAlbum);
+    searchForm.addEventListener('submit', Album.search);
+
 
 
 });
